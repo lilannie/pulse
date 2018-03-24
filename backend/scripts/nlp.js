94e3236cd88c0025ceb4bbc41cf047120b7f7a1b
Joe Kuczek <joe.kuczek@gmail.com>
2018-03-24 16:30:59 -0500

    added nlp script

--- backend/scripts/nlp.js
+++ backend/scripts/nlp.js
@@ -1,12 +1,25 @@
-const classifier = new require('natural-brain').BrainJSClassifier();
+var BrainJSClassifier = require('natural-brain');
+const db = require('../mongo/config/database');
+var classifier = new BrainJSClassifier();
+const Votable = require('../mongo/models/citizen_model');
 
+db.connect().then(db => {
+  db.model('Votable').find({}, (doc) => {
+    console.log(doc);
+  });
+});
+ 
 classifier.addDocument('my unit-tests failed.', 'software');
 classifier.addDocument('tried the program, but it was buggy.', 'software');
 classifier.addDocument('tomorrow we will do standup.', 'meeting');
+classifier.addDocument('the drive has a 2TB capacity.', 'hardware');
+classifier.addDocument('i need a new power supply.', 'hardware');
 classifier.addDocument('can you play some new music?', 'music');
-
+ 
 classifier.train();
-
+ 
 console.log(classifier.classify('did the tests pass?')); // -> software
+console.log(classifier.classify('did you buy a new drive?')); // -> hardware
+console.log(classifier.classify('What is the capacity?')); // -> hardware
 console.log(classifier.classify('Lets meet tomorrow?')); // -> meeting
 console.log(classifier.classify('Can you play some stuff?')); // -> music
