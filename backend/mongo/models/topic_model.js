d5d5d3695cd48eea8936ef29c5d1519520e9737c
qmsalas <qmsalas@iastate.edu>
2018-03-24 15:52:46 -0500

    wip: topics

--- backend/mongo/models/topic_model.js
+++ backend/mongo/models/topic_model.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const Topic = new mongoose.Schema({
+  ID: Number,
   title: String
 });
 
--- backend/scripts/insert_topics.js
+++ backend/scripts/insert_topics.js
@@ -1,3 +1,5 @@
+const Topic = require('../mongo/models/topic_model');
+
 // A script that manually inserts the following list of topics...
 
 /**
@@ -12,3 +14,16 @@
  * {id: , title: Elections}
  * ]
  */
+
+Topic.insertMany([
+  { id: 1, title: 'Gun Control' },
+  { id: 2, title: 'Trump' },
+  { id: 3, title: 'Trade War' },
+  { id: 4, title: 'Immigration' },
+  { id: 5, title: 'Taxes' },
+  { id: 6, title: 'Russian Hacking' },
+  { id: 7, title: 'Facebook Data' },
+  { id: 8, title: 'Elections' }
+]).then(result => {
+  console.log(result);
+});
