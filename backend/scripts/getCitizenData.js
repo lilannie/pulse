bc50a4a98a902d82f1505e6ffc0b3bb9f718db15
Annie Steenson <anniesteenso@gmail.com>
2018-03-24 14:33:51 -0500

    Fixed issue with saving to the database

--- backend/scripts/getCitizenData.js
+++ backend/scripts/getCitizenData.js
@@ -5,82 +5,79 @@ const votable_data = require('./votables');
 const db = require('../mongo/config/database');
 const votable_ctrl = require('../mongo/controllers/votable_ctrl');
 
-db.connect();
+db.connect().then(db => {
+	console.log('Connected');
 
-db.on('open', () => {
-  const votables = votable_data.votables;
-  const input_path = 'citizen.csv';
-  const demographics = [
-    { key: 'age', no_convert: true },
-    'state',
-    'sex',
-    'irace',
-    'marital',
-    'relig',
-    'income',
-    'party',
-    'citizen',
-    'educ2',
-    'ideo'
-  ];
+	const votables = votable_data.votables;
+	const input_path = 'citizen.csv';
+	const demographics = [
+		{ key: 'age', no_convert: true },
+		'state',
+		'sex',
+		'irace',
+		'marital',
+		'relig',
+		'income',
+		'party',
+		'citizen',
+		'educ2',
+		'ideo'
+	];
 
-  setTimeout(() => {
-    db.collection.insertMany(votables, (error, doc) => {
-      console.log(error, doc);
-    });
-  }, 5000)
-  
-  /* Return/select all votables in order to obtain votable ID for votes */
+	db.model('Votable').insertMany(votables, (error, doc) => {
+		console.log(error, doc);
+	});
 
-  let citizens = [];
-  csv()
-    .fromFile(input_path)
-    .on('json', row => {
-      let citizen_data = {};
-
-      for (prop of demographics) {
-        if (prop.no_convert) {
-          citizen_data[prop.key] = row[prop.key];
-        } else {
-          citizen_data[prop] = convertDemographic(prop, row);
-        } // end if dont convert
-      } // end foreach prop
-
-      /* TODO: Insert Citizen into MongoDB 
-      get citizen_id as return value and use for vote */
-      citizens.push(citizen_data);
-
-      for (votable of votables) {
-        let choices = votable.choices;
-        let choice = convertChoice(votable.col, row, choices);
-
-        /* TODO: Store vote in Blockchain???? */
-        let vote = {
-          citizen_id: 0,
-          votable_id: 0,
-          choice: choice
-        };
-        //console.log(vote);
-      } // end for loop over votables
-      //process.exit();
-    });
-
-  setTimeout(() => {
-    db.collection.insertMany(citizens);
-  }, 20000)
-
-  const convertDemographic = (key, row) => {
-    let data = row[key];
-    data--;
-    return data > labels[key].length || data < 0 || isNaN(data) ? "Don't Know" : labels[key][data];
-  };
-
-  const convertChoice = (key, row, choices) => {
-    let data = row[key];
-    data--;
-    return data > choices.length || data < 0 || isNaN(data) ? "Don't Know" : choices[data];
-  };
+	// /* Return/select all votables in order to obtain votable ID for votes */
+	//
+	// let citizens = [];
+	// csv()
+	// 	.fromFile(input_path)
+	// 	.on('json', row => {
+	// 		let citizen_data = {};
+	//
+	// 		for (prop of demographics) {
+	// 			if (prop.no_convert) {
+	// 				citizen_data[prop.key] = row[prop.key];
+	// 			} else {
+	// 				citizen_data[prop] = convertDemographic(prop, row);
+	// 			} // end if dont convert
+	// 		} // end foreach prop
+	//
+	// 		/* TODO: Insert Citizen into MongoDB
+	// 		get citizen_id as return value and use for vote */
+	// 		citizens.push(citizen_data);
+	//
+	// 		for (votable of votables) {
+	// 			let choices = votable.choices;
+	// 			let choice = convertChoice(votable.col, row, choices);
+	//
+	// 			/* TODO: Store vote in Blockchain???? */
+	// 			let vote = {
+	// 				citizen_id: 0,
+	// 				votable_id: 0,
+	// 				choice: choice
+	// 			};
+	// 			//console.log(vote);
+	// 		} // end for loop over votables
+	// 		//process.exit();
+	// 	});
+	//
+	// setTimeout(() => {
+	// 	db.collection.insertMany(citizens);
+	// }, 20000)
+	//
+	// const convertDemographic = (key, row) => {
+	// 	let data = row[key];
+	// 	data--;
+	// 	return data > labels[key].length || data < 0 || isNaN(data) ? "Don't Know" : labels[key][data];
+	// };
+	//
+	// const convertChoice = (key, row, choices) => {
+	// 	let data = row[key];
+	// 	data--;
+	// 	return data > choices.length || data < 0 || isNaN(data) ? "Don't Know" : choices[data];
+	// };
 });
-
 
 
