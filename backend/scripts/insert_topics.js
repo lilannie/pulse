e3b0d51a721cfeb9f7f96a6bd6ef4e633282303f
qmsalas <qmsalas@iastate.edu>
2018-03-24 16:30:06 -0500

    adding topics works

--- backend/scripts/insert_topics.js
+++ backend/scripts/insert_topics.js
@@ -1,29 +1,30 @@
 const Topic = require('../mongo/models/topic_model');
-
+const db = require('../mongo/config/database');
 // A script that manually inserts the following list of topics...
-
 /**
  * [
- * {id: , title: Gun Control},
- * {id: , title: Trump},
- * {id: , title: Trade War},
- * {id: , title: Immigration},
- * {id: , title: Taxes},
- * {id: , title: Russian Hacking},
- * {id: , title: Facebook Data},
- * {id: , title: Elections}
+ * {_custom_id: 1, title: Gun Control},
+ * {_custom_id: 2, title: Trump},
+ * {_custom_id: 3, title: Trade War},
+ * {_custom_id: 4, title: Immigration},
+ * {_custom_id: 5, title: Taxes},
+ * {_custom_id: 6, title: Russian Hacking},
+ * {_custom_id: 7, title: Facebook Data},
+ * {_custom_id: 8, title: Elections}
  * ]
  */
 
-Topic.insertMany([
-  { id: 1, title: 'Gun Control' },
-  { id: 2, title: 'Trump' },
-  { id: 3, title: 'Trade War' },
-  { id: 4, title: 'Immigration' },
-  { id: 5, title: 'Taxes' },
-  { id: 6, title: 'Russian Hacking' },
-  { id: 7, title: 'Facebook Data' },
-  { id: 8, title: 'Elections' }
-]).then(result => {
-  console.log(result);
+db.connect().then(() => {
+  Topic.insertMany([
+    { _custom_id: 1, title: 'Gun Control' },
+    { _custom_id: 2, title: 'Trump' },
+    { _custom_id: 3, title: 'Trade War' },
+    { _custom_id: 4, title: 'Immigration' },
+    { _custom_id: 5, title: 'Taxes' },
+    { _custom_id: 6, title: 'Russian Hacking' },
+    { _custom_id: 7, title: 'Facebook Data' },
+    { _custom_id: 8, title: 'Elections' }
+  ]).then(result => {
+    console.log('Added Topics!');
+  });
 });
