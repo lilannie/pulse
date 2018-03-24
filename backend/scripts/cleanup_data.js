c322f8e8b016d33b44fec02a03dae60a31068b9d
qmsalas <qmsalas@iastate.edu>
2018-03-24 00:02:26 -0500

    mv scripts dir. minor refactoring

--- backend/scripts/cleanup_data.js
+++ backend/scripts/cleanup_data.js
@@ -7,12 +7,12 @@ let cursor = 28001;
 let cursors = [];
 let all_questions = [];
 
-const getItems = (cursor) => {
+const getItems = cursor => {
   //console.log('cursor'+cursor);
   return new Promise((resolve, reject) => {
-    request(base_url + 'polls?cursor='+cursor, (error, response, body) => {
+    request(base_url + 'polls?cursor=' + cursor, (error, response, body) => {
       if (error || body == undefined || body == null) {
-        reject(new Error("End of reading input"));
+        reject(new Error('End of reading input'));
       }
 
       try {
@@ -21,46 +21,40 @@ const getItems = (cursor) => {
         reject(new Error());
       }
 
-      if(!cursors.includes(cursor)){
+      if (!cursors.includes(cursor)) {
         //console.log(body.next_cursor);
         let polls = body.items;
 
-        if(polls){
-          for(poll of polls) {
+        if (polls) {
+          for (poll of polls) {
             let poll_questions = poll.poll_questions;
-  
-            for(item of poll_questions){
+
+            for (item of poll_questions) {
               // pull out the question and the response data
               let description = item.text;
               let responses = item.question.responses;
               let sample_pop = item.sample_subpopulations;
-              
-              if(description && !all_questions.includes(description)){
-                
-               // console.log(question);
+
+              if (description && !all_questions.includes(description)) {
+                // console.log(question);
 
                 //let id = 5;
                 let votable = {
                   //id: 5,
                   description: description,
-                  responses: responses.map((response) => response.name)
-                }
+                  responses: responses.map(response => response.name)
+                };
 
-                for(sample of sample_pop) {
-
+                for (sample of sample_pop) {
                   console.log(sample);
                 }
-  
+
                 console.log(votable);
-                
-  
               }
-  
-            }// end foreach poll questions
-  
-          }// end foreach polls
+            } // end foreach poll questions
+          } // end foreach polls
         }
-        
+
         // keep track of all cursors so we don't count dupes
         cursors.push(cursor);
       }
@@ -70,12 +64,12 @@ const getItems = (cursor) => {
       }, 5000);
     });
   });
-}
+};
 
 //while(cursor > 1) {
-  getItems(cursor).catch((error) => {
-    console.log(error);
-    process.exit();
-  });
-  cursor -= 15;
+getItems(cursor).catch(error => {
+  console.log(error);
+  process.exit();
+});
+cursor -= 15;
 //}
