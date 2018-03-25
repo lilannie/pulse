f236b96c0ef65a94f36144fd4093f1b01b986254
lilannie <anniesteenso@gmail.com>
2018-03-24 23:33:58 -0500

    Fixed citizen script

--- backend/scripts/load_data.js
+++ backend/scripts/load_data.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const nlp = require('compromise');
 const db = require('../mongo/config/database');
 const votable_data = require('./votables');
-const votable_ctrl = require('../mongo/controllers/votable_ctrl');
 const csv = require('csvtojson');
 const labels = require('./conversions');
 const Votable = require('../mongo/models/citizen_model');
@@ -45,7 +44,7 @@ const createCitizen = (citizen_model, citizen) => new Promise((resolve, reject) 
 		.then(response => {
 			citizen.blockchainID = response.newUserAddress;
 
-			citizen_model.create(console, (error, newCitizen) => {
+			citizen_model.create(citizen, (error, newCitizen) => {
 				resolve(newCitizen);
 			});
 		})
@@ -148,121 +147,4 @@ db.connect().then(async (db) => {
 		.catch(error => {
 			console.log(error);
 		});
-
-
-
-  //
-  // for(let i = 0; i < votables.length; i++) {
-  //   let votable = votables[i];
-  //
-  //   let choices = votable.choices;
-  //   let votable_id = 0;
-  //
-  //   /* Create the blockchain contract for each votable */
-  //   let contract = {
-  //     itemID: i,
-  //     responses: choices
-  //   };
-  //
-  //   request.post({
-  //     url: 'http://localhost:3333/contract/create',
-  //     method: "POST",
-  //     headers: {
-  //       'Content-Type': 'application/json'
-  //     },
-  //     body: JSON.stringify(contract)
-  //   }, (error, data) => {
-  //     if(!data || error != null) return;
-  //
-  //     let json = JSON.parse(data.body);
-  //     votable.contract_id = json.data.minedAddress;
-  //
-  //     delete votable.col;
-  //     console.log(votable);
-  //     db.model('Votable').create(votable, (error, new_votable) => {
-  //       votable = new_votable;
-  //
-  //       const convertDemographic = (key, row) => {
-  //         let data = row[key];
-  //         data--;
-  //         return data > labels[key].length || data < 0 || isNaN(data) ? "Don't Know" : labels[key][data];
-  //       };
-  //
-  //       const convertChoice = (key, row, choices) => {
-  //         let data = row[key];
-  //         data--;
-  //         return data > choices.length || data < 0 || isNaN(data) ? "Don't Know" : choices[data];
-  //       };
-  //
-  //       csv().fromFile(input_path).on('json', row => {
-  //         // loop over content of .csv file
-  //         let citizen_data = { blockchainID: 0, demographicInfo: {} };
-  //
-  //         // loop over each demographic prop
-  //         for (prop of demographics) {
-  //           let real_name = '';
-  //
-  //           if(prop.name != null){
-  //             real_name = prop.name;
-  //           }else{
-  //             real_name = prop.key;
-  //           }
-  //
-  //           if (prop.no_convert) {
-  //             citizen_data.demographicInfo[real_name] = row[prop.key];
-  //           } else {
-  //             citizen_data.demographicInfo[real_name] = convertDemographic(prop.key, row);
-  //           } // end if dont convert
-  //         } // end foreach prop
-  //
-  //         // make a get request to create a blockchain user
-  //         request.get('http://localhost:3333/create/user', async (error, data) => {
-  //           if(!data || error != null) return;
-  //
-  //           let json = JSON.parse(data.body);
-  //           let newUserAddress = json.newUserAddress;
-  //           citizen_data.blockchainID = newUserAddress;
-  //
-  //           console.log(citizen_data);
-  //           /* Create a citizen */
-  //           db.model('Citizen').create(citizen_data).then(citizen => {
-  //             /* Handle votes for a citizen */
-  //             let choices = votable.choices;
-  //             let choice = convertChoice(temp_votables[i].col, row, choices);
-  //             let votable_id = votable._id;
-  //
-  //             /* Store vote in Blockchain */
-  //             let vote = {
-  //               voterAddress: newUserAddress,
-  //               contractAddress: votable.contract_id,
-  //               response: choice
-  //             };
-  //
-  //             /* Make a final post request creating the vote */
-  //             request.post({
-  //                 url: 'http://localhost:3333/contract/vote',
-  //                 method: "POST",
-  //                 headers: {
-  //                   'Content-Type': 'application/json'
-  //                 },
-  //                 body: JSON.stringify(vote)
-  //               }, async (error, doc) => {
-  //                 if(!doc || error != null) return;
-  //                 console.log('ERROR on request side')
-  //                 console.log(error);
-  //                 console.log(doc);
-  //                 await sleep(1500);
-  //             });
-  //           });
-  //           await sleep(1500);
-  //         });
-  //       });
-  //     });
-  //   });
-  //   await sleep(1500);
-  // }// end foreach loop over votables
 });
-
-const sleep = (ms) => {
-	return new Promise(resolve => setTimeout(resolve, ms));
-}
