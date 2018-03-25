2ffb35512ae39302258281d9b92487b9ad37916d
qmsalas <qmsalas@iastate.edu>
2018-03-25 06:01:19 -0500

    add missed file

--- backend/mongo/config/database.js
+++ backend/mongo/config/database.js
@@ -2,5 +2,5 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 exports.connect = () => {
-	return mongoose.connect('mongodb://localhost:27017/hackathon');
+  return mongoose.connect('mongodb://localhost:27017/hackathon');
 };
